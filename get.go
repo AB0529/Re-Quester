d5d745ab88f0eab4c4661bd046e0f55242f53805
AB0529 <supercrab115@gmail.com>
2020-07-19 16:49:08 -0400

    Initial commit

---
+++ get.go
@@ -0,0 +1,6 @@
+package main
+
+// Get will perfrom a GET request on a given url
+func Get(url *string) {
+	return
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// ReURL custom url defination
+type ReURL struct {
+	Host   string `json:"host"`
+	Port   string `json:":port"`
+	Params map[string]string
+}
+
+func main() {
+	fmt.Println("Wadup nigga higga mugga shigga ")
+}
